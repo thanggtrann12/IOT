a9c268c18f5562cbafb5b93f766045db62dd4bd7
Thanggtrann12 <thanggtrann12@gmail.com>
2021-04-11 09:57:36 +0700

    hjp

--- json/account.js
+++ json/account.js
@@ -10,7 +10,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     if(user != null){
 
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      alert("welcome"+email_id);
 
     }
 
