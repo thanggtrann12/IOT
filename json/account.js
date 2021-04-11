fee370aa4d513c4597395409eb8c5d9c44d66e13
Thanggtrann12 <thanggtrann12@gmail.com>
2021-04-11 09:54:30 +0700

    hjp

--- json/account.js
+++ json/account.js
@@ -1,61 +1,45 @@
 firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-  
-     
-      var user = firebase.auth().currentUser;
-  
-      if(user != null){
-  
-        
-        window.location.href="/IOT/templates/sidebar.html";
-      }
-  
-    } else {
-      // No user is signed in.
-  
-      document.getElementById("username").style.display = "block";
-      document.getElementById("password").style.display = "block";
-  
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
     }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
   });
-  
-  function login(){
-  
-    var userEmail = document.getElementById("username").value;
-    var userPass = document.getElementById("password").value;
-  
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-     
-      // ...
-    });
-  
-  }
-  
-  function logout(){
-    firebase.auth().signOut();
-  }
-  function signUp(){
-          
-    var userEmail = document.getElementById("email_field").value;
-    var userPass = document.getElementById("password_field").value;
-    
-    firebase.auth().createUserWithEmailAndPassword(userEmail,userPass).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-  
-      window.alert("Error : " + errorMessage);
-  
-      // ...
-    });
-    
-    alert("Signed Up");
-  }
-  
-  
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
