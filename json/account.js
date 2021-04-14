161dc940b587892e20aad4c42d6bb4681fbcf506
thangtran <thanggtrann12@gmail.com>
2021-04-14 16:47:46 +0700

    s

--- json/account.js
+++ json/account.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var user = ['thanggtrann12'];
 var pass = ['0393807319'];
             function login()
@@ -18,88 +17,16 @@ var pass = ['0393807319'];
          
           function signup()
           {
-              user.push(document.getElementById("username_sp").value);
-              if (document.getElementById("password_sp").value==document.getElementById("password_cf").value)
+              
+              if (document.getElementById("password_sp").value == document.getElementById("password_cf").value)
               {
+              user.push(document.getElementById("username_sp").value);
               pass.push(document.getElementById("password_sp").value);
+              console.log("ok");
               window.location.href = "/IOT/index.html";
               }
               else alert("Password donot match");
               window.location.href = "/IOT/templates/signup.html";
              
           }
-=======
-// firebase.auth().onAuthStateChanged(function(user) {
-//   if (user) {
-//     // User is signed in.
 
-//     document.getElementById("user_div").style.display = "block";
-//     document.getElementById("login_div").style.display = "none";
-//     window.location.href="/IOT/templates/sidebar.html"
-//     window.location.assign="/IOT/templates/sidebar.html"
-//     var user = firebase.auth().currentUser;
-
-//     if(user != null){
-
-//       var email_id = user.email;
-//       alert("welcome"+email_id);
-
-//     }
-
-//   } else {
-//     // No user is signed in.
-
-//     document.getElementById("user_div").style.display = "none";
-//     document.getElementById("login_div").style.display = "block";
-
-//   }
-// });
-
-// function login(){
-
-//   var userEmail = document.getElementById("email_field").value;
-//   var userPass = document.getElementById("password_field").value;
-
-//   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-
-//     window.alert("Error : " + errorMessage);
-
-//     // ...
-//   });
-
-// }
-
-// function logout(){
-//   firebase.auth().signOut();
-// }
-// function signUp(){
-
-//   var email = document.getElementById("email");
-//   var password = document.getElementById("password");
-
-//   firebase.auth().createUserWithEmailAndPassword(email.value, password.value);
-
-//   alert("Signed Up");
-// }
-
-var user = ['thanggtrann12'];
-var pass = ['0393807319'];
-function login() {
-  for (var i = 0; i < user.length; i++)
-    if (user[i] == document.getElementById("username").value && pass[i] == document.getElementById("password").value) {
-      window.location.href = "/IOT/templates/sidebar.html";
-
-    }
-    else {
-      alert("Your account incorrect");
-    }
-}
-function signup() {
-  user.push(document.getElementById("username").value);
-  pass.push(document.getElementById("password").value);
-
-}
->>>>>>> e4806bb019b658a8b73aee7fe87b3a668989f956
