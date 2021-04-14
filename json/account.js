b95230ee4414e14082d97f970730fc57e678c4fe
buiminhquang188 <buiminhquang188@gmail.com>
2021-04-14 20:45:06 +0700

    Update account.js

--- json/account.js
+++ json/account.js
@@ -2,9 +2,10 @@ var user = ['thanggtrann12'];
 var pass = ['1'];
 function login() {
     for (var i = 0; i < user.length; i++)
-        if (user[i] == document.getElementById("username").value && pass[i] == document.getElementById("password").value) {
+        if (user[i] == document.getElementById("username").value && pass[i] == document.getElementById("password").value) 
+        {
             console.log("in coming");
-            location.replace("https://thanggtrann12.github.io/IOT/templates/sidebar.html")
+            location.replace("https://thanggtrann12.github.io/IOT/templates/sidebar.html");
         }
         else {
             alert("Sai acc");
