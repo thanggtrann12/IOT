e0014a0859d0bfbd44da7f3e7b617a5f98543432
thangtran <thanggtrann12@gmail.com>
2021-04-14 22:56:25 +0700

    s

--- json/account.js
+++ json/account.js
@@ -1,5 +1,5 @@
-var user = ['thanggtrann12'];
-var pass = ['1'];
+ user = ['thanggtrann12'];
+ pass = ['1'];
 function login() {
     for (var i = 0; i < user.length; i++)
         if (user[i] == document.getElementById("username").value && pass[i] == document.getElementById("password").value) 
