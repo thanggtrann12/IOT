dea010c7b0b62f6fc898a5ba006d8b9a7fa1455a
buiminhquang188 <buiminhquang188@gmail.com>
2021-04-14 20:39:42 +0700

    Update account.js

--- json/account.js
+++ json/account.js
@@ -1,35 +1,29 @@
 var user = ['thanggtrann12'];
 var pass = ['1'];
-            function login()
-                {
-                for (var i = 0; i<user.length;i++)
-                if (user[i] == document.getElementById("username").value && pass[i] == document.getElementById("password").value) 
-                 {
-                  console.log("in coming");
-                    window.location.href = "https://thanggtrann12.github.io/IOT/templates/sidebar.html";
-                }
-               else 
-                {
-                 alert("Sai acc"); 
-                 window.location.reload();           
-                }
-          }
-         
-          function signup()
-          {
-              
-              if (document.getElementById("password_sp").value == document.getElementById("password_cf").value)
-              {
-              user.push(document.getElementById("username_sp").value);
-              pass.push(document.getElementById("password_sp").value);
-              console.log("ok");
-              document.location = "/IOT/templates/index.html";
-              }
-              else 
-              {
-                  alert("Password donot match");
-              document.location = "/IOT/templates/signup.html";
-              }
-             
-          }
+function login() {
+    for (var i = 0; i < user.length; i++)
+        if (user[i] == document.getElementById("username").value && pass[i] == document.getElementById("password").value) {
+            console.log("in coming");
+            location.replace("https://thanggtrann12.github.io/IOT/templates/sidebar.html")
+        }
+        else {
+            alert("Sai acc");
+            window.location.reload();
+        }
+}
+
+function signup() {
+
+    if (document.getElementById("password_sp").value == document.getElementById("password_cf").value) {
+        user.push(document.getElementById("username_sp").value);
+        pass.push(document.getElementById("password_sp").value);
+        console.log("ok");
+        document.location = "/IOT/templates/index.html";
+    }
+    else {
+        alert("Password donot match");
+        document.location = "/IOT/templates/signup.html";
+    }
+
+}
 
