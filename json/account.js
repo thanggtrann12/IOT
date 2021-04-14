daf35bb60aec2eed3edbb31fdb26bd1f69709ecc
buiminhquang188 <buiminhquang188@gmail.com>
2021-04-14 16:19:37 +0700

    Update account.js

--- json/account.js
+++ json/account.js
@@ -5,7 +5,6 @@ firebase.auth().onAuthStateChanged(function(user) {
     document.getElementById("user_div").style.display = "block";
     document.getElementById("login_div").style.display = "none";
     window.location.href="/IOT/templates/sidebar.html"
-    function login()
     window.location.assign="/IOT/templates/sidebar.html"
     var user = firebase.auth().currentUser;
 
