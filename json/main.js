4516c3d9cd68ce4a8243060252275b48f8313520
Quang <buiminhquang188@gmail.com>
2021-05-16 10:04:13 +0700

    Xếp ngay ngắn lại thoi

--- json/main.js
+++ json/main.js
@@ -1,30 +1,28 @@
 
 
-    function logout()
-    {
-        firebase.auth().signOut().then(function(){
-            console.log('success');
-            window.location.replace("/IOT/templates/login.html");
-        })
-    }
-
- function init()
-{
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          // User is signed in.
-          console.log("stay");
-          //window.location.replace("/IOT/templates/sidebar.html");
-          
-        } else {
-          // No user is signed in.
-        
-          console.log("redirect");
-          window.location.replace("/IOT/templates/login.html");
-        }
-      });
+function logout() {
+  firebase.auth().signOut().then(function () {
+    console.log('success');
+    window.location.replace("/IOT/templates/login.html");
+  })
 }
-    
+
+function init() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      console.log("stay");
+      //window.location.replace("/IOT/templates/sidebar.html");
+
+    } else {
+      // No user is signed in.
+
+      console.log("redirect");
+      window.location.replace("/IOT/templates/login.html");
+    }
+  });
+}
+
 init();
 
 
